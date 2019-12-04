02cfc7a2faa624ceccd416b0029786e535380df0
Usama M. Ali <eng.usama.muhammad@gmail.com>
2019-12-04 16:43:04 +0100

    remove other page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
 import Fib from './Fib';
 
 class App extends Component {
@@ -14,11 +13,9 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fib Calculator K8S</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
           </div>
         </div>
       </Router>
